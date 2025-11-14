66263131800dd2bc3001420f32787e8f30a9a558
Bruno Oliveira <bruno.doliveira@sptech.school>
2025-11-13 21:02:46 -0300

    feat: prevendo dados de farmacia e perfumarias

--- server.py
+++ server.py
@@ -3,9 +3,11 @@ from random_forest import RandomForestModel
 
 model_roupas = RandomForestModel()
 model_alimentos = RandomForestModel()
+model_farmacia = RandomForestModel()
 
 model_roupas.treinar_modelo("data/vendas_roupas.csv")
 model_alimentos.treinar_modelo("data/hipermercado.csv", "2014")
+model_farmacia.treinar_modelo("data/farmacia_cosmeticos.csv", "2018")
 
 app = Flask(__name__)
 
@@ -33,6 +35,18 @@ def previsao_alimentos(ano, mes):
         ano = int(ano)
 
         previsao = str(model_alimentos.prever(mes, ano))
+    except Exception as e:
+        return e.args[0]
+    else:
+        return previsao
+    
+@app.route("/obter_previsao/farmacia/<ano>/<mes>")
+def previsao_farmacia(ano, mes):
+    try:
+        mes = int(mes)
+        ano = int(ano)
+
+        previsao = str(model_farmacia.prever(mes, ano))
     except Exception as e:
         return e.args[0]
     else:
