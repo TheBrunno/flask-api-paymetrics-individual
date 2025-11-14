6982999bc8e4bd374e8c6a8905e852dce81e4820
Bruno Oliveira <bruno.doliveira@sptech.school>
2025-11-13 22:38:20 -0300

    feat: prever venda de moveis e eletrodomesticos

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from random_forest import RandomForestModel
 from get_bucket import obterCSVsDoS3
 
 arquivos = [
-    "vendas_roupas", "hipermercado", "farmacia_cosmeticos"
+    "vendas_roupas", "hipermercado", "farmacia_cosmeticos", "moveis_eletrodomesticos"
 ]
 
 obterCSVsDoS3(arquivos)
@@ -12,10 +12,12 @@ obterCSVsDoS3(arquivos)
 model_roupas = RandomForestModel()
 model_alimentos = RandomForestModel()
 model_farmacia = RandomForestModel()
+model_moveis = RandomForestModel()
 
 model_roupas.treinar_modelo(f"data/{arquivos[0]}_bucket.csv")
 model_alimentos.treinar_modelo(f"data/{arquivos[1]}_bucket.csv", "2014")
 model_farmacia.treinar_modelo(f"data/{arquivos[2]}_bucket.csv", "2018")
+model_moveis.treinar_modelo(f"data/{arquivos[3]}_bucket.csv", "2008")
 
 app = Flask(__name__)
 
@@ -55,6 +57,18 @@ def previsao_farmacia(ano, mes):
         ano = int(ano)
 
         previsao = str(model_farmacia.prever(mes, ano))
+    except Exception as e:
+        return e.args[0]
+    else:
+        return previsao
+    
+@app.route("/obter_previsao/moveis/<ano>/<mes>")
+def previsao_moveis(ano, mes):
+    try:
+        mes = int(mes)
+        ano = int(ano)
+
+        previsao = str(model_moveis.prever(mes, ano))
     except Exception as e:
         return e.args[0]
     else:
