4afbc0d98298b53d8867ce975dab3d0a52b14c19
Bruno Oliveira <bruno.doliveira@sptech.school>
2025-11-14 21:22:07 -0300

    feat: pegando dados da api do ipea e enviando para o bucket

---
+++ ipeadata.py
@@ -0,0 +1,31 @@
+# vai p um lambda na aws -> responsavel por atualizar os csvs do bucket
+
+import requests
+import pandas as pd
+import boto3
+
+cod_series = [
+    "PMC12_VNSUPTN12",
+    "PMC12_VNFARMN12",
+    "PMC12_VNVESTN12",
+    "PMC12_VRELETRN12"
+]
+
+def send_bucket(file_name, bucket, object_name):
+    s3 = boto3.client('s3')
+    s3.upload_file(file_name, bucket, object_name)
+
+for cod_serie in cod_series:
+    response = requests.get(f"https://www.ipeadata.gov.br/api/odata4/ValoresSerie(SERCODIGO='{cod_serie}')").json()
+
+    data = {
+        "Data": [],
+        "valor": []
+    }
+
+    for value in response["value"]:
+        data["Data"].append(value["VALDATA"])
+        data["valor"].append(value["VALVALOR"])
+
+    pd.DataFrame(data=data).to_csv(f"data_ipea/{cod_serie}.csv", index=False)
+    send_bucket(f"data_ipea/{cod_serie}.csv", "raw-paymetrics", f"bruno/ipea/{cod_serie}.csv")
