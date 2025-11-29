bf0f75772838162c02e840d4639882f277f99fca
Bruno Oliveira <bruno.doliveira@sptech.school>
2025-11-29 20:29:48 -0300

    fix: corrigindo falta de criação automatica de pasta

--- get_bucket.py
+++ get_bucket.py
@@ -1,7 +1,9 @@
 import boto3
+import os
 
 def obterCSVsDoS3(arquivos:list):
     s3 = boto3.client('s3')
 
+    os.makedirs("data/", exist_ok=True)
     for arquivo in arquivos:
         s3.download_file('raw-paymetrics', f'bruno/ipea/{arquivo}.csv', f'data/{arquivo}_bucket.csv')
